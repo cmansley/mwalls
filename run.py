8d6112e5c54794dec7f835570ab83d3213b1be6c
Chris Mansley <cmansley@cs.rutgers.edu>
2011-03-22 13:33:27 -0400

    Changed run into a multi-trial framework

--- run.py
+++ run.py
@@ -4,36 +4,51 @@ from blocking import BlockingWorld
 from shortcut import ShortcutWorld
 from dyna import DynaQ
 
-# Experimental Framework 
-env = BlockingWorld()
-#env = ShortcutWorld()
+def eval(outfile, env, epsilon, init):
 
-output = []
+    f = open(outfile, "w")
 
-for j in range(50):
+    output = []
+
+    for j in range(50):
+        
+        cumrew = 0.0
+        s = env.ss()
+        alg = DynaQ(epsilon, init)
+        
+        output.append([])
+
+        for i in range(env.len()):
+            
+            a = alg.policy(s)
     
-    cumrew = 0.0
-    s = env.ss()
-    alg = DynaQ(0.001, 0)
+            sp, r = env.step(s,a,i)
 
-    output.append([])
+            alg.learn(s,a,r,sp)
+
+            cumrew += r
+
+            s = sp
+
+            output[j].append(cumrew)
+
 
     for i in range(env.len()):
-        
-        a = alg.policy(s)
-    
-        sp, r = env.step(s,a,i)
+        for j in range(50):
+            f.write(str(output[j][i]) + ',')
+        f.write('\n')
 
-        alg.learn(s,a,r,sp)
-
-        cumrew += r
-
-        s = sp
-
-        output[j].append(cumrew)
+    f.close()
 
 
-for i in range(env.len()):
-    for j in range(50):
-        print output[j][i],',',
-    print
+epsilons = [0, 0.001]
+aname = ['m','p']
+inits = [0, 0.9, 1, 2, 10]
+envs = [BlockingWorld(), ShortcutWorld()]
+ename = ['bl', 'sc']
+
+for i in range(len(envs)):
+    for j in range(len(epsilons)):
+        for k in inits:
+            filename = 'dyna_' + aname[j] + '_' + str(k) + '_' + ename[i] + '.txt'
+            eval(filename, envs[i], epsilons[j], k)
